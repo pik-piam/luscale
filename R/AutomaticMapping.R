63f7dddf9aadb720da3307aacff9078f5abdf5dd
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2018-11-15 16:42:14 +0100

    updated authors

--- R/AutomaticMapping.R
+++ R/AutomaticMapping.R
@@ -1,16 +1,16 @@
 #' AutomaticMapping
 #' 
-#' ???
+#' Function automatically finds fitting mapping for provided MAgPIE object for a given target aggregation.
 #' 
 #' 
 #' @usage AutomaticMapping(x,mapping=NULL,from=NULL,to=NULL)
-#' @param x ???
-#' @param mapping ???
+#' @param x MAgPIE object
+#' @param mapping A array or data.frame containing a mapping query or mapping name
 #' @param from Only required if query is not NULL. Column of the query with
 #' original dimnames of the incoming dataset
 #' @param to Only required if query is not NULL. Column of the query with the
 #' target dimnames of the outcoming dataset
-#' @author ???
+#' @author Benjamin Leon Bodirsky
 #' @export
 #' @importFrom utils data read.csv
 #' @export
