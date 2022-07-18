ac916bfce02c841749bf173e1dd54115c534068b
mppalves <mppalves@gmail.com>
2022-07-18 11:19:43 +0200

    update on pastures dissagregation functions

--- R/PastrMngtShares.R
+++ R/PastrMngtShares.R
@@ -10,37 +10,42 @@
 #' 
 #'  Equations
 #'  X[i] * C[i] = cost
-#'  sum(X[i]) == avl_area
-#'  X[i] * Y[i] == production
+#'  sum(X[i]) >= avl_area * (1 - eps)
+#'  sum(X[i]) <= avl_area * (1 + eps)
+#'  X[i] * Y[i] >= production * (1 - eps)
+#'  X[i] * Y[i] <= production * (1 + eps)
 #'
 #' Where: 
 #' X : Pasture management areas
 #' C : cost of production per type of management
 #' Y : Pasture yields per type of managment 
 #' 
-#' @usage PastrMngtShares(cost, yields, avl_area, production)
+#' @usage PastrMngtShares(cost, yields, avl_area, production, eps)
 #' @param cost Magpie object with cellular cost for each pasture management type
 #' @param yields Magpie object with cellular yields for each management type
 #' @param avl_area Magpie object with cellular data on the total managed pasture areas available
 #' @param production Magpie object with cellular data on the total pasture demand
+#' @param eps epsilon Percent relaxation for area and production
+#' 
 #' @return Mapie object with the total pasture areas optimized to fulfill pasture demand in the available areas. An extra dimension is added to
 #' capture cells that are unfeasible.
 #' @author Marcos Alves
 #' @examples
 #' \dontrun{
-#' PastrMngtShares(cost, yields, avl_area, production)
+#' PastrMngtShares(cost, yields, avl_area, production, eps)
 #' }
 #'
 #' @importFrom lpSolve lp
 #' @export
 #'
-PastrMngtShares <- function(cost, yields, avl_area, production) {
+
+PastrMngtShares <- function(cost, yields, avl_area, production, eps) {
   y <-
     array(
       data = 0,
-      dim = c(dim(yields)[c(1, 2)], dim(yields)[3] + 1),
+      dim = c(dim(yields)[c(1, 2)], dim(yields)[3] + 2),
       dimnames = c(dimnames(yields)[c(1, 2)], list("out" = append(
-        dimnames(yields)[[3]], "infes"
+        dimnames(yields)[[3]], c("excess_bio", "infes_local")
       )))
     )
   if (!length(getItems(cost, dim = 3)) == length(getItems(yields, dim = 3))) {
@@ -58,32 +63,41 @@ PastrMngtShares <- function(cost, yields, avl_area, production) {
   for (year in getItems(x, dim = 2)) {
     print(paste0("Optimizing year: ", year))
     core_opt <- function(x) {
-      infes <- 0
+      excess_bio <- infes_local <- 0
       i <- length(x)
       n <- (i - 2) / 2
+      
+      # if production = 0, skip optmization to save time.
       if ((x[i - 1] != 0)) {
         f.obj <- x[seq_len(n)]
         f.con <- rbind(
           rep(1, n),
+          rep(1, n),
+          x[seq(n + 1, n * 2, 1)],
           x[seq(n + 1, n * 2, 1)]
         )
         f.dir <- c(
-          "==",
-          "=="
+          ">=",
+          "<=",
+          ">=",
+          "<="
         )
         f.rhs <- c(
-          x[i - 1],
-          x[i]
+          x[i - 1] * (1 - eps),
+          x[i - 1] * (1 + eps),
+          x[i] * (1 - eps),
+          x[i] * (1 + eps)
         )
-        res <- lp("min", f.obj, f.con, f.dir, f.rhs)
+        res <- lpSolve::lp("min", f.obj, f.con, f.dir, f.rhs)
         if (res$status == 2) {
           j <- match(max(x[seq(n + 1, n * 2, 1)]), x[seq(n + 1, n * 2, 1)])
           res$solution[j] <- x[i - 1]
-          infes <- 1
+          excess_bio <- x[i] - sum(x[seq(n + 1, n * 2, 1)] * res$solution)
+          infes_local <- 1
         }
-        return(c(res$solution, infes))
+        return(c(res$solution, excess_bio, infes_local))
       }
-      return(rep(0, n + 1))
+      return(rep(0, n + 2))
     }
     z <- unlist(apply(x[, year, ], 1, core_opt))
     z <- t(z)
